c6fe82a5b7db613af065cbcd9d4b7212f8744bea
Damien Davis-Neff <damiendavisneff@gmail.com>
2023-04-19 13:17:49 -0400

    It now works on different drives

--- Mods.bat
+++ Mods.bat
@@ -1,15 +1,23 @@
 @echo off
 
-cd "C:\Program Files (x86)\Steam\steamapps\common\Grand Theft Auto V"
+set diri=C:\Program Files (x86)\Steam\steamapps\common\Grand Theft Auto V
+echo CURRENT GAME DIRECTORY: "%diri%"
+echo
+echo CHECKING MOD STATUS
 
-IF EXIST "dinput8.dll" (
-    ren dinput8.dll _dinput8.dll
+IF EXIST "%diri%\dinput8.dll" (
+    echo MODS CURRENTLY ENABLED
+    echo DISABLING THEM
+    ren "%diri%\dinput8.dll" "_dinput8.dll"
     echo MODS DISABLED
-) ELSE IF EXIST "_dinput8.dll" (
-    ren _dinput8.dll dinput8.dll
+) ELSE IF EXIST "%diri%\_dinput8.dll" (
+    echo MODS CURRENTLY DISABLED
+    echo ENABLING THEM
+    ren "%diri%\_dinput8.dll" "dinput8.dll"
     echo MODS ENABLED
 ) ELSE (
     echo MODS NOT FOUND
+    echo ENSURE DIRECTORY IS CORRECT
 )
 
 pause
